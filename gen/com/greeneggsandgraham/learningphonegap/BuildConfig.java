1a3c99563f7f558c35a583a22d99bdb2bca90571
jasonisgraham <jgraham84@gmail.com>
2013-02-19 16:01:27 -0500

    removing ignroed files

--- gen/com/greeneggsandgraham/learningphonegap/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.greeneggsandgraham.learningphonegap;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
